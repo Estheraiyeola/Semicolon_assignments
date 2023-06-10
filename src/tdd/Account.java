8275cb7d2ad76a953918f48e8e867c91793700fd
Estheraiyeola <estheraiyeola@yahoo.com>
2023-06-10 16:21:21 +0100

    Added an Account program

---
+++ src/tdd/Account.java
@@ -0,0 +1,31 @@
+package tdd;
+
+public class Account {
+    private int balance;
+    private String pin;
+
+    public Account(String pin){
+        this.pin = pin;
+    }
+
+    public int getBalance(String pin) {
+        if(this.pin != pin)return 0;
+        this.pin = pin;
+        return balance;
+    }
+
+    public void deposit(int amount) {
+        if(amount < 0) return;
+        balance += amount;
+        //boolean amountIsPositive = amount > 0;
+        //if(amountIsPositive) balance += amount;
+    }
+
+    public void withdraw(int amount, String pin) {
+        if(amount < 0) return;
+        balance -= amount;
+        if(this.pin != pin)return;
+        this.pin = pin;
+    }
+
+}
