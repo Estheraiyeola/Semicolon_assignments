b0cd3b61e823e1eb25214e7511e7c8b45d433a74
Estheraiyeola <estheraiyeola@yahoo.com>
2023-06-23 05:58:53 +0100

    Updated the withdrawal, deposit and getBalance methods

--- src/tdd/Account.java
+++ src/tdd/Account.java
@@ -1,30 +1,46 @@
 package tdd;
 
+import java.util.Objects;
+
 public class Account {
     private int balance;
-    private String pin;
+    private final String pin;
 
     public Account(String pin){
         this.pin = pin;
     }
 
     public int getBalance(String pin) {
-        if(this.pin != pin)return 0;
-        this.pin = pin;
-        return balance;
+        if (Objects.equals(this.pin, pin)){
+            return balance;
+        }
+        else {
+            System.out.println("Incorrect pin");
+            return 0;
+        }
     }
 
     public void deposit(int amount) {
-        if(amount < 0) return;
-        balance += amount;
+        if (amount > 0){
+            balance += amount;
+        }
+        else {
+            System.out.println("Can't Deposit That Amount");
+        }
     }
 
     public void withdraw(int amount, String pin) {
-        if(amount < 0) return;
-        if (amount > balance) return;
-        balance -= amount;
-        if(this.pin != pin)return;
-        this.pin = pin;
+        if (Objects.equals(this.pin, pin)){
+            if (amount > 0 && amount <= balance){
+                balance -= amount;
+            }
+            else {
+                System.out.println("Insufficient Balance");
+            }
+        }
+        else{
+            System.out.println("Incorrect Pin");
+        }
     }
 
 
